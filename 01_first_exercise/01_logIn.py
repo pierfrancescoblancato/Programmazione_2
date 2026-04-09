3d7d266accb62408a1a3cb9f3f87d0385146f9b0
pierfrancescoblancato <pierfrancescoblancato436@gmail.com>
2026-04-09 15:04:47 +0200

    perf: add attemps variable

--- 01_first_exercise/01_logIn.py
+++ 01_first_exercise/01_logIn.py
@@ -1,19 +1,18 @@
 username = "admin"
 password = "123abc"
-
+Attempts = 3
 access = False
 
-for i in range(3):
+for i in range(Attempts):
     userIn = input("Enter the username: ")
     passIn = input("Enter the password: ")
 
     clearPassIn = passIn.strip().lower()
-
     if username == userIn and password == clearPassIn:
         access = True
         break
     else:
-        print(f"LogIn: ERROR! Attempts remaining: {2 - i}")
+        print(f"LogIn: ERROR! Attempts remaining: {(Attempts-1) - i}")
 
 if access:
     print("LogIn: Successful!")
